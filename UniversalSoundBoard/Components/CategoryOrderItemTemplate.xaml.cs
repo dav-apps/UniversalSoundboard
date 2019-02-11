4d3c2dd7c0a1043623c487e6c7e3b477f656eb92
David <David@TOSHIBALAPTOP>
2019-02-12 00:11:12 +0100

    Add basic C ategoryOrderItemTemplate

---
+++ UniversalSoundBoard/Components/CategoryOrderItemTemplate.xaml.cs
@@ -0,0 +1,16 @@
+﻿using UniversalSoundBoard.Models;
+using Windows.UI.Xaml.Controls;
+
+namespace UniversalSoundboard.Components
+{
+    public sealed partial class CategoryOrderItemTemplate : UserControl
+    {
+        public Category Category { get { return DataContext as Category; } }
+
+        public CategoryOrderItemTemplate()
+        {
+            InitializeComponent();
+            DataContextChanged += (s, e) => Bindings.Update();
+        }
+    }
+}
