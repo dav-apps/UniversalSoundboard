ae7bf7dbce6bcdd8fc2c6b30d06202dfff437375
David <David@TOSHIBALAPTOP>
2019-03-09 20:21:17 +0100

    Add TestInit method and write tests for GetAllSounds

--- UniversalSoundboard.Tests/DataAccess/DatabaseOperationsTest.cs
+++ UniversalSoundboard.Tests/DataAccess/DatabaseOperationsTest.cs
@@ -4,22 +4,36 @@ using davClassLibrary.Models;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Threading.Tasks;
 using UniversalSoundboard.Tests.Common;
 using UniversalSoundBoard.DataAccess;
 
 namespace UniversalSoundboard.Tests.DataAccess
 {
-    [TestClass]
+    [TestClass][DoNotParallelize]
     public class DatabaseOperationsTest
     {
         [ClassInitialize]
-        public static void Setup(TestContext context)
+        public static void ClassInit(TestContext context)
         {
             ProjectInterface.RetrieveConstants = new RetrieveConstants();
             ProjectInterface.GeneralMethods = new GeneralMethods();
             ProjectInterface.LocalDataSettings = new LocalDataSettings();
             ProjectInterface.TriggerAction = new TriggerAction();
+        }
+
+        [TestInitialize]
+        public async Task TestInit()
+        {
+            // Delete all files and folders in the test folder except the database file
+            var davFolder = new DirectoryInfo(FileManager.GetDavDataPath());
+            foreach (var folder in davFolder.GetDirectories())
+                folder.Delete(true);
+            
+            // Clear the database
+            var database = new davClassLibrary.DataAccess.DavDatabase();
+            await database.DropAsync();
         }
 
         #region GetObject
@@ -164,6 +178,76 @@ namespace UniversalSoundboard.Tests.DataAccess
             Assert.AreEqual(name, tableObjectFromDatabase.GetPropertyValue(FileManager.SoundTableNamePropertyName));
             Assert.AreEqual(false, bool.Parse(tableObjectFromDatabase.GetPropertyValue(FileManager.SoundTableFavouritePropertyName)));
             Assert.IsNull(tableObjectFromDatabase.GetPropertyValue(FileManager.SoundTableCategoryUuidPropertyName));
+        }
+        #endregion
+
+        #region GetAllSounds
+        [TestMethod]
+        public async Task GetAllSoundShouldReturnAllSounds()
+        {
+            // Arrange
+            var firstSoundUuid = Guid.NewGuid();
+            var secondSoundUuid = Guid.NewGuid();
+            var firstSoundFileUuid = Guid.NewGuid();
+            var secondSoundFileUuid = Guid.NewGuid();
+            string firstSoundName = "Phoenix Objection";
+            string secondSoundName = "Godot Hold it";
+
+            List<string> firstSoundCategoryUuids = new List<string>
+            {
+                Guid.NewGuid().ToString(),
+                Guid.NewGuid().ToString()
+            };
+            List<string> secondSoundCategoryUuids = new List<string>
+            {
+                Guid.NewGuid().ToString()
+            };
+
+            // Create the sounds
+            await DatabaseOperations.AddSoundAsync(firstSoundUuid, firstSoundName, firstSoundFileUuid.ToString(), firstSoundCategoryUuids);
+            await DatabaseOperations.AddSoundAsync(secondSoundUuid, secondSoundName, secondSoundFileUuid.ToString(), secondSoundCategoryUuids);
+
+            // Act
+            List<TableObject> sounds = await DatabaseOperations.GetAllSoundsAsync();
+
+            // Assert
+            Assert.AreEqual(2, sounds.Count);
+
+            // Test the first table object
+            Assert.AreEqual(firstSoundUuid, sounds[0].Uuid);
+            Assert.AreEqual(firstSoundFileUuid, Guid.Parse(sounds[0].GetPropertyValue(FileManager.SoundTableSoundUuidPropertyName)));
+            Assert.AreEqual(firstSoundName, sounds[0].GetPropertyValue(FileManager.SoundTableNamePropertyName));
+
+            int i = 0;
+            string[] firstTableObjectCategoryUuids = sounds[0].GetPropertyValue(FileManager.SoundTableCategoryUuidPropertyName).Split(",");
+            foreach (var categoryUuid in firstSoundCategoryUuids)
+            {
+                Assert.AreEqual(Guid.Parse(categoryUuid), Guid.Parse(firstTableObjectCategoryUuids[i]));
+                i++;
+            }
+
+            // Test the second table object
+            Assert.AreEqual(secondSoundUuid, sounds[1].Uuid);
+            Assert.AreEqual(secondSoundFileUuid, Guid.Parse(sounds[1].GetPropertyValue(FileManager.SoundTableSoundUuidPropertyName)));
+            Assert.AreEqual(secondSoundName, sounds[1].GetPropertyValue(FileManager.SoundTableNamePropertyName));
+
+            i = 0;
+            string[] secondTableObjectCategoryUuids = sounds[1].GetPropertyValue(FileManager.SoundTableCategoryUuidPropertyName).Split(",");
+            foreach (var categoryUuid in secondSoundCategoryUuids)
+            {
+                Assert.AreEqual(Guid.Parse(categoryUuid), Guid.Parse(secondTableObjectCategoryUuids[i]));
+                i++;
+            }
+        }
+
+        [TestMethod]
+        public async Task GetAllSoundsShouldReturnEmptyListIfThereAreNoSounds()
+        {
+            // Act
+            List<TableObject> sounds = await DatabaseOperations.GetAllSoundsAsync();
+
+            // Assert
+            Assert.AreEqual(0, sounds.Count);
         }
         #endregion
     }
