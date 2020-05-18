ac3b9bb3b64f9917742a88797a894c48b3ba0f3f
Dav <dav2070@web.de>
2020-05-18 15:27:07 +0200

    Add basic Sound ListView
    - Add SoundListItemTemplate with the name of the Sound
    - Add new Pivot in SoundPage with the ListViews

---
+++ UniversalSoundBoard/Components/SoundListItemTemplate.xaml.cs
@@ -0,0 +1,23 @@
+﻿using UniversalSoundBoard.DataAccess;
+using UniversalSoundBoard.Models;
+using Windows.UI.Xaml.Controls;
+
+namespace UniversalSoundboard.Components
+{
+    public sealed partial class SoundListItemTemplate : UserControl
+    {
+        public Sound Sound { get => DataContext as Sound; }
+
+        public SoundListItemTemplate()
+        {
+            InitializeComponent();
+            DataContextChanged += (s, e) => Bindings.Update();
+            SetDataContext();
+        }
+
+        private void SetDataContext()
+        {
+            ContentRoot.DataContext = FileManager.itemViewHolder;
+        }
+    }
+}
