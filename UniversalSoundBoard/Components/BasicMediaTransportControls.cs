50c80fbef3128a18d9e8a8ff6ee79cd36bba57b2
Dav <dav2070@web.de>
2020-07-18 17:50:10 +0200

    Copy and adapt the MediaTransportControls style and remove all buttons
    - Create BasicMediaTransportControls style with only the progress bar and the time text elements
    - Create BasicMediaTransportControls class using the style
    - Add the style to MergedDictionaries in App.xaml

---
+++ UniversalSoundBoard/Components/BasicMediaTransportControls.cs
@@ -0,0 +1,13 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace UniversalSoundboard.Components
+{
+    public sealed class BasicMediaTransportControls : MediaTransportControls
+    {
+        public BasicMediaTransportControls()
+        {
+            Style = (Style)Application.Current.Resources["BasicMediaTransportControls"];
+        }
+    }
+}
