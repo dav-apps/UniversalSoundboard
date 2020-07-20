0f90b7859b564ad6291ef2071269e3344bab323c
Dav <dav2070@web.de>
2020-07-20 22:17:22 +0200

    Create basic VolumeMenuFlyoutItem class and overridden style
    - Override the MenuFlyoutItem style, remove the icon and text and add button and slider
    - Add VolumeMenuFlyoutItem class using the style

---
+++ UniversalSoundBoard/Components/VolumeMenuFlyoutItem.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Input;
+
+namespace UniversalSoundboard.Components
+{
+    public class VolumeMenuFlyoutItem : MenuFlyoutItem
+    {
+        public event EventHandler<RoutedEventArgs> MuteButton_Click;
+        public event EventHandler<RangeBaseValueChangedEventArgs> VolumeSlider_ValueChanged;
+
+        public VolumeMenuFlyoutItem()
+        {
+            Style = (Style)Application.Current.Resources["VolumeMenuFlyoutItem"];
+        }
+
+        protected override void OnApplyTemplate()
+        {
+            Button MuteButton = GetTemplateChild("MuteButton") as Button;
+            MuteButton.Click += (object sender, RoutedEventArgs e) => MuteButton_Click?.Invoke(sender, e);
+
+            Slider VolumeSlider = GetTemplateChild("VolumeSlider") as Slider;
+            VolumeSlider.ValueChanged += (object sender, RangeBaseValueChangedEventArgs e) => VolumeSlider_ValueChanged?.Invoke(sender, e);
+
+            base.OnApplyTemplate();
+        }
+
+        protected override void OnPointerPressed(PointerRoutedEventArgs e){}
+
+        protected override void OnTapped(TappedRoutedEventArgs e){}
+    }
+}
