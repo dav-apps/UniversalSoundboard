00a740818a0568519b4974c6cd214335a4d65590
Dav <dav2070@web.de>
2020-07-21 19:19:05 +0200

    Create VolumeControl with button and slider
    - Add events for ValueChanged and FocusLost and add the ability to set the value of the Slider

---
+++ UniversalSoundBoard/Components/VolumeControl.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace UniversalSoundboard.Components
+{
+    public sealed partial class VolumeControl : UserControl
+    {
+        public event EventHandler<RangeBaseValueChangedEventArgs> ValueChanged;
+        public new event EventHandler<RoutedEventArgs> LostFocus;
+
+        public double Value
+        {
+            get => VolumeSlider.Value;
+            set => VolumeSlider.Value = value;
+        }
+
+        public VolumeControl()
+        {
+            InitializeComponent();
+        }
+
+        private void VolumeSlider_ValueChanged(object sender, RangeBaseValueChangedEventArgs e) => ValueChanged?.Invoke(sender, e);
+
+        private void VolumeSlider_LostFocus(object sender, RoutedEventArgs e) => LostFocus?.Invoke(sender, e);
+    }
+}
