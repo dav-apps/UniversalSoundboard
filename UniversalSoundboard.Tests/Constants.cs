eaca5483abb588f406e8b4c72ccfc442d3173676
Dav <dav2070@web.de>
2021-03-14 15:13:14 +0100

    Update constants with access token and add utils with setup methods

--- UniversalSoundboard.Tests/Constants.cs
+++ UniversalSoundboard.Tests/Constants.cs
@@ -1,7 +1,7 @@
 ﻿namespace UniversalSoundboard.Tests
 {
-    public static class Constants
+    internal static class Constants
     {
-        public const string Jwt = "eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6ImRhdmNsYXNzbGlicmFyeXRlc3RAZGF2LWFwcHMudGVjaCIsInVzZXJuYW1lIjoiZGF2Q2xhc3NMaWJyYXJ5VGVzdFVzZXIiLCJ1c2VyX2lkIjoxMiwiZGV2X2lkIjoyLCJleHAiOjM3NTI5MTgzODQxfQ.lO-iq5UHk25wnysbrEw1PirgGBhz-rFqrK6iRGkcFnU";
+        internal const string testerXTestAppAccessToken = "ckktuu0gs00008iw3ctnrofzf";
     }
 }
---
+++ UniversalSoundboard.Tests/Utils.cs
@@ -0,0 +1,52 @@
+﻿using davClassLibrary;
+using davClassLibrary.Common;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using UniversalSoundboard.DataAccess;
+using UniversalSoundboard.Tests.Common;
+
+namespace UniversalSoundboard.Tests
+{
+    internal class Utils
+    {
+        internal static void GlobalSetup()
+        {
+            ProjectInterface.LocalDataSettings = new LocalDataSettings();
+            ProjectInterface.Callbacks = new Callbacks();
+            FileManager.itemViewHolder = new UniversalSoundboard.Common.ItemViewHolder();
+
+            Dav.Init(
+                Environment.Test,
+                FileManager.AppId,
+                new List<int>
+                {
+                    FileManager.OrderTableId,
+                    FileManager.CategoryTableId,
+                    FileManager.SoundTableId,
+                    FileManager.SoundFileTableId,
+                    FileManager.PlayingSoundTableId,
+                    FileManager.ImageFileTableId
+                },
+                new List<int>
+                {
+                    FileManager.SoundTableId,
+                    FileManager.SoundFileTableId
+                },
+                FileManager.GetDavDataPath()
+            );
+        }
+
+        internal static async Task Setup()
+        {
+            // Delete all files and folders in the test folder except the database file
+            var davFolder = new DirectoryInfo(FileManager.GetDavDataPath());
+            foreach (var folder in davFolder.GetDirectories())
+                folder.Delete(true);
+
+            // Clear the database
+            var database = new davClassLibrary.DataAccess.DavDatabase();
+            await database.DropAsync();
+        }
+    }
+}
