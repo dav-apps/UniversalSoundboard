d3d0502ef3db45b231493991b78e5c141e45d200
David Zurek <dav2070@web.de>
2022-06-08 18:04:49 +0200

    Add simple UI for selecting input device for sound recorder

--- UniversalSoundBoard/Pages/SoundRecorderPage.xaml.cs
+++ UniversalSoundBoard/Pages/SoundRecorderPage.xaml.cs
@@ -1,19 +1,32 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
 using UniversalSoundboard.Common;
 using UniversalSoundboard.DataAccess;
+using UniversalSoundboard.Models;
+using Windows.Devices.Enumeration;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
 namespace UniversalSoundboard.Pages
 {
     public sealed partial class SoundRecorderPage : Page
     {
+        DeviceWatcherHelper deviceWatcherHelper = new DeviceWatcherHelper(DeviceClass.AudioCapture);
+        DeviceInfo inputDevice;
+
         public SoundRecorderPage()
         {
             InitializeComponent();
 
-            RootGrid.DataContext = FileManager.itemViewHolder;
+            ContentRoot.DataContext = FileManager.itemViewHolder;
 
             FileManager.itemViewHolder.PropertyChanged += ItemViewHolder_PropertyChanged;
+            deviceWatcherHelper.DevicesChanged += DeviceWatcherHelper_DevicesChanged;
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            UpdateInputDeviceComboBox();
         }
 
         private void ItemViewHolder_PropertyChanged(object sender, PropertyChangedEventArgs e)
@@ -24,6 +37,40 @@ namespace UniversalSoundboard.Pages
                     RequestedTheme = FileManager.GetRequestedTheme();
                     break;
             }
+        }
+
+        private void DeviceWatcherHelper_DevicesChanged(object sender, EventArgs e)
+        {
+            UpdateInputDeviceComboBox();
+        }
+
+        private void InputDeviceComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var inputDeviceId = (InputDeviceComboBox.SelectedItem as ComboBoxItem).Tag;
+            int i = deviceWatcherHelper.Devices.FindIndex(d => d.Id.Equals(inputDeviceId));
+
+            if (i != -1)
+                inputDevice = deviceWatcherHelper.Devices[i];
+        }
+
+        private void UpdateInputDeviceComboBox()
+        {
+            InputDeviceComboBox.Items.Clear();
+            int selectedItemIndex = -1;
+
+            for (int i = 0; i < deviceWatcherHelper.Devices.Count; i++)
+            {
+                var device = deviceWatcherHelper.Devices[i];
+                if (inputDevice != null && inputDevice.Id == device.Id) selectedItemIndex = i;
+
+                InputDeviceComboBox.Items.Add(new ComboBoxItem
+                {
+                    Content = device.Name,
+                    Tag = device.Id
+                });
+            }
+
+            InputDeviceComboBox.SelectedIndex = selectedItemIndex;
         }
     }
 }
