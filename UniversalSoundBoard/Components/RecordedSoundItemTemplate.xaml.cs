ed1ab9b617434253222d143cbb2bf8934c8ee38d
David Zurek <dav2070@web.de>
2022-06-16 01:15:12 +0200

    Add simple RecordedSoundItem and RecordedSoundItemTemplate for displaying sounds in recorder

---
+++ UniversalSoundBoard/Components/RecordedSoundItemTemplate.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using UniversalSoundboard.Models;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace UniversalSoundboard.Components
+{
+    public sealed partial class RecordedSoundItemTemplate : UserControl
+    {
+        public RecordedSoundItem RecordedSoundItem { get { return DataContext as RecordedSoundItem; } }
+
+        public RecordedSoundItemTemplate()
+        {
+            InitializeComponent();
+            DataContextChanged += (s, e) => Bindings.Update();
+        }
+    }
+}
---
+++ UniversalSoundBoard/Models/RecordedSoundItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace UniversalSoundboard.Models
+{
+    public class RecordedSoundItem
+    {
+        public string Name { get; set; }
+        public StorageFile File { get; set; }
+
+        public RecordedSoundItem(string name, StorageFile file)
+        {
+            Name = name;
+            File = file;
+        }
+    }
+}
