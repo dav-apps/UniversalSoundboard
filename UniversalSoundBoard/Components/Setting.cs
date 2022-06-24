2b0330cb1d02606be986061a83af3021510535c3
David Zurek <dav2070@web.de>
2022-06-24 22:42:58 +0200

    Implement Setting control for modern settings entries
    - Update nuget packages in app and test projects

---
+++ UniversalSoundBoard/Components/Setting.cs
@@ -0,0 +1,29 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace UniversalSoundboard.Components
+{
+    public class Setting : ContentControl
+    {
+        public Setting()
+        {
+            DefaultStyleKey = typeof(Setting);
+            Style = (Style)Application.Current.Resources["SettingStyle"];
+        }
+
+        public static readonly DependencyProperty HeaderProperty = DependencyProperty.Register("Header", typeof(string), typeof(Setting), null);
+        public static readonly DependencyProperty ActionContentProperty = DependencyProperty.Register("ActionContent", typeof(object), typeof(Setting), null);
+
+        public string Header
+        {
+            get => (string)GetValue(HeaderProperty);
+            set => SetValue(HeaderProperty, value);
+        }
+
+        public object ActionContent
+        {
+            get => GetValue(ActionContentProperty);
+            set => SetValue(ActionContentProperty, value);
+        }
+    }
+}
