aa702a128e04323d04d8d604cf98a1696d13f222
David Zurek <dav2070@web.de>
2022-07-03 23:50:26 +0200

    Add simple Dialog class
    - Implement Dialog class as a base representation of a ContentDialog, which can be inherited to create specific ContentDialogs

---
+++ UniversalSoundBoard/Models/Dialog.cs
@@ -0,0 +1,49 @@
+﻿using System.Threading.Tasks;
+using UniversalSoundboard.Common;
+using UniversalSoundboard.DataAccess;
+using Windows.Foundation;
+using Windows.UI.Xaml.Controls;
+
+namespace UniversalSoundboard.Models
+{
+    public class Dialog
+    {
+        private ContentDialog ContentDialog { get; }
+        public event TypedEventHandler<ContentDialog, ContentDialogButtonClickEventArgs> PrimaryButtonClick;
+        public event TypedEventHandler<ContentDialog, ContentDialogButtonClickEventArgs> CloseButtonClick;
+
+        public Dialog(string title, string content, string closeButtonText)
+        {
+            ContentDialog = new ContentDialog
+            {
+                Title = title,
+                Content = content,
+                CloseButtonText = closeButtonText,
+                RequestedTheme = FileManager.GetRequestedTheme()
+            };
+
+            ContentDialog.CloseButtonClick += (ContentDialog sender, ContentDialogButtonClickEventArgs args) => CloseButtonClick?.Invoke(sender, args);
+        }
+
+        public Dialog(string title, string content, string primaryButtonText, string closeButtonText, ContentDialogButton defaultButton = ContentDialogButton.Primary)
+        {
+            ContentDialog = new ContentDialog
+            {
+                Title = title,
+                Content = content,
+                PrimaryButtonText = primaryButtonText,
+                CloseButtonText = closeButtonText,
+                DefaultButton = defaultButton,
+                RequestedTheme = FileManager.GetRequestedTheme()
+            };
+
+            ContentDialog.PrimaryButtonClick += (ContentDialog sender, ContentDialogButtonClickEventArgs args) => PrimaryButtonClick?.Invoke(sender, args);
+            ContentDialog.CloseButtonClick += (ContentDialog sender, ContentDialogButtonClickEventArgs args) => CloseButtonClick?.Invoke(sender, args);
+        }
+
+        public async Task ShowAsync()
+        {
+            await ContentDialogs.ShowContentDialogAsync(ContentDialog);
+        }
+    }
+}
