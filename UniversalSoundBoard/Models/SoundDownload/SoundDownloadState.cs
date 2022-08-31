e62d2e70d5551f10fa898a556ab3bebfa97f4a64
David Zurek <dav2070@web.de>
2022-08-31 23:42:33 +0200

    Implement sound download state classes for saving playlist download progress

---
+++ UniversalSoundBoard/Models/SoundDownload/SoundDownloadState.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace UniversalSoundboard.Models
+{
+    public class SoundDownloadState
+    {
+        private const string SoundDownloadStateFileName = "soundDownloadState.json";
+
+        public int CurrentIndex { get; set; }
+        public Guid CategoryUuid { get; set; }
+
+        public SoundDownloadState(int currentIndex, Guid categoryUuid)
+        {
+            CurrentIndex = currentIndex;
+            CategoryUuid = categoryUuid;
+        }
+
+        public string ToJson()
+        {
+            return JsonSerializer.Serialize(this);
+        }
+
+        public async Task Save()
+        {
+            StorageFolder localCacheFolder = ApplicationData.Current.LocalCacheFolder;
+            StorageFile soundDownloadStateFile = await localCacheFolder.TryGetItemAsync(SoundDownloadStateFileName) as StorageFile;
+
+            if (soundDownloadStateFile == null)
+                soundDownloadStateFile = await localCacheFolder.CreateFileAsync(SoundDownloadStateFileName);
+
+            await FileIO.WriteTextAsync(soundDownloadStateFile, ToJson());
+        }
+
+        public static async Task<SoundDownloadState> Load()
+        {
+            StorageFolder localCacheFolder = ApplicationData.Current.LocalCacheFolder;
+
+            StorageFile soundDownloadStateFile = await localCacheFolder.TryGetItemAsync(SoundDownloadStateFileName) as StorageFile;
+            if (soundDownloadStateFile == null) return null;
+
+            string stateJson = await FileIO.ReadTextAsync(soundDownloadStateFile);
+            if (stateJson == null) return null;
+
+            return JsonSerializer.Deserialize<SoundDownloadState>(stateJson);
+        }
+    }
+}
---
+++ UniversalSoundBoard/Models/SoundDownload/SoundDownloadStateItems.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace UniversalSoundboard.Models
+{
+    public class SoundDownloadStateItems
+    {
+        private const string SoundDownloadStateItemsFileName = "soundDownloadStateItems.json";
+
+        public string Class { get; set; }
+        public List<SoundDownloadItem> SoundItems { get; set; }
+
+        public SoundDownloadStateItems(List<SoundDownloadItem> soundItems)
+        {
+            SoundItems = soundItems;
+
+            if (soundItems.Count > 0)
+                Class = SoundItems.First().GetType().Name;
+            else
+                Class = typeof(SoundDownloadItem).Name;
+        }
+
+        public string ToJson()
+        {
+            return JsonSerializer.Serialize(this);
+        }
+
+        public async Task Save()
+        {
+            StorageFolder localCacheFolder = ApplicationData.Current.LocalCacheFolder;
+            StorageFile soundDownloadStateFile = await localCacheFolder.TryGetItemAsync(SoundDownloadStateItemsFileName) as StorageFile;
+
+            if (soundDownloadStateFile == null)
+                soundDownloadStateFile = await localCacheFolder.CreateFileAsync(SoundDownloadStateItemsFileName);
+
+            await FileIO.WriteTextAsync(soundDownloadStateFile, ToJson());
+        }
+
+        public static async Task<SoundDownloadStateItems> Load()
+        {
+            StorageFolder localCacheFolder = ApplicationData.Current.LocalCacheFolder;
+
+            StorageFile soundDownloadStateFile = await localCacheFolder.TryGetItemAsync(SoundDownloadStateItemsFileName) as StorageFile;
+            if (soundDownloadStateFile == null) return null;
+
+            string stateJson = await FileIO.ReadTextAsync(soundDownloadStateFile);
+            if (stateJson == null) return null;
+
+            return JsonSerializer.Deserialize<SoundDownloadStateItems>(stateJson);
+        }
+    }
+}
