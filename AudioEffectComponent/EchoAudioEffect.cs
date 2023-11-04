adbcf6e437e3a07965ac45da388ac28f35ed6ae6
David <zurek.dav@gmail.com>
2023-11-04 20:44:55 +0100

    Add Echo effect to AudioEffectComponent

---
+++ AudioEffectComponent/EchoAudioEffect.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Media;
+using Windows.Media.Effects;
+using Windows.Media.MediaProperties;
+
+namespace AudioEffectComponent
+{
+    public sealed class EchoAudioEffect : IBasicAudioEffect
+    {
+        private float[] echoBuffer;
+        private int currentActiveSampleIndex;
+        private AudioEncodingProperties currentEncodingProperties;
+        IPropertySet configuration;
+
+        public bool TimeIndependent { get { return true; } }
+        public bool UseInputFrameForOutput { get { return false; } }
+
+        // Volume of the Echo, between 0 and 1
+        public float Volume
+        {
+            get
+            {
+                object val;
+
+                if (configuration != null && configuration.TryGetValue("Volume", out val))
+                    return (float)val;
+
+                return 1f;
+            }
+        }
+
+        // Delay in milliseconds
+        public int Delay
+        {
+            get
+            {
+                object val;
+
+                if (configuration != null && configuration.TryGetValue("Delay", out val))
+                    return (int)val;
+
+                return 1000;
+            }
+        }
+
+        public IReadOnlyList<AudioEncodingProperties> SupportedEncodingProperties
+        {
+            get
+            {
+                var supportedEncodingProperties = new List<AudioEncodingProperties>();
+                AudioEncodingProperties encodingProps1 = AudioEncodingProperties.CreatePcm(44100, 1, 32);
+                encodingProps1.Subtype = MediaEncodingSubtypes.Float;
+                AudioEncodingProperties encodingProps2 = AudioEncodingProperties.CreatePcm(48000, 1, 32);
+                encodingProps2.Subtype = MediaEncodingSubtypes.Float;
+
+                supportedEncodingProperties.Add(encodingProps1);
+                supportedEncodingProperties.Add(encodingProps2);
+
+                return supportedEncodingProperties;
+
+            }
+        }
+
+        public void SetEncodingProperties(AudioEncodingProperties encodingProperties)
+        {
+            currentEncodingProperties = encodingProperties;
+            echoBuffer = new float[(int)(encodingProperties.SampleRate * ((double)Delay / 1000))]; // exactly one second delay
+            currentActiveSampleIndex = 0;
+        }
+
+        public void SetProperties(IPropertySet configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        [ComImport]
+        [Guid("5B0D3235-4DBA-4D44-865E-8F1D0E4FD04D")]
+        [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+        unsafe interface IMemoryBufferByteAccess
+        {
+            void GetBuffer(out byte* buffer, out uint capacity);
+        }
+
+        unsafe public void ProcessFrame(ProcessAudioFrameContext context)
+        {
+            AudioFrame inputFrame = context.InputFrame;
+            AudioFrame outputFrame = context.OutputFrame;
+
+            using (
+                AudioBuffer inputBuffer = inputFrame.LockBuffer(AudioBufferAccessMode.Read),
+                outputBuffer = outputFrame.LockBuffer(AudioBufferAccessMode.Write)
+            )
+            using (
+                IMemoryBufferReference inputReference = inputBuffer.CreateReference(),
+                outputReference = outputBuffer.CreateReference()
+            )
+            {
+                byte* inputDataInBytes;
+                byte* outputDataInBytes;
+                uint inputCapacity;
+                uint outputCapacity;
+
+                ((IMemoryBufferByteAccess)inputReference).GetBuffer(out inputDataInBytes, out inputCapacity);
+                ((IMemoryBufferByteAccess)outputReference).GetBuffer(out outputDataInBytes, out outputCapacity);
+
+                float* inputDataInFloat = (float*)inputDataInBytes;
+                float* outputDataInFloat = (float*)outputDataInBytes;
+
+                float inputData;
+                float echoData;
+
+                // Process audio data
+                int dataInFloatLength = (int)inputBuffer.Length / sizeof(float);
+
+                for (int i = 0; i < dataInFloatLength; i++)
+                {
+                    inputData = inputDataInFloat[i] * (1.0f - (Volume / 2));
+                    echoData = echoBuffer[currentActiveSampleIndex] * (Volume / 2);
+                    outputDataInFloat[i] = inputData + echoData;
+                    echoBuffer[currentActiveSampleIndex] = inputDataInFloat[i];
+                    currentActiveSampleIndex++;
+
+                    if (currentActiveSampleIndex == echoBuffer.Length)
+                    {
+                        // Wrap around (after one second of samples)
+                        currentActiveSampleIndex = 0;
+                    }
+                }
+            }
+        }
+
+        public void Close(MediaEffectClosedReason reason)
+        {
+            // Dispose of effect resources
+            echoBuffer = null;
+        }
+
+        public void DiscardQueuedFrames()
+        {
+            // Reset contents of the samples buffer
+            Array.Clear(echoBuffer, 0, echoBuffer.Length - 1);
+            currentActiveSampleIndex = 0;
+        }
+    }
+}
