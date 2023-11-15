9c0385eb2e6695d1688b034262e74968081e9a94
David <zurek.dav@gmail.com>
2023-11-15 14:51:24 +0100

    Add Play / Pause button with functionality for playing the sound on StoreSoundPage

--- UniversalSoundBoard/Pages/StoreSoundPage.xaml.cs
+++ UniversalSoundBoard/Pages/StoreSoundPage.xaml.cs
@@ -1,24 +1,59 @@
-﻿using UniversalSoundboard.Models;
+﻿using System;
+using UniversalSoundboard.DataAccess;
+using UniversalSoundboard.Models;
+using Windows.Media.Core;
+using Windows.Media.Playback;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
 namespace UniversalSoundboard.Pages
 {
     public sealed partial class StoreSoundPage : Page
     {
-        private string name = "";
+        private SoundResponse soundItem;
+        private MediaPlayer mediaPlayer;
 
         public StoreSoundPage()
         {
             InitializeComponent();
+            mediaPlayer = new MediaPlayer();
+            mediaPlayer.MediaEnded += MediaPlayer_MediaEnded;
+        }
+
+        private void Page_Loaded(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            SetThemeColors();
         }
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
             base.OnNavigatedTo(e);
-            var soundItem = e.Parameter as SoundResponse;
-            name = soundItem.Name;
+
+            if (e.Parameter == null)
+                return;
+
+            soundItem = e.Parameter as SoundResponse;
             Bindings.Update();
+
+            mediaPlayer.Source = MediaSource.CreateFromUri(new Uri(soundItem.AudioFileUrl));
+        }
+
+        private void MediaPlayer_MediaEnded(MediaPlayer sender, object args)
+        {
+
+        }
+
+        private void SetThemeColors()
+        {
+            RequestedTheme = FileManager.GetRequestedTheme();
+            SolidColorBrush appThemeColorBrush = new SolidColorBrush(FileManager.GetApplicationThemeColor());
+            ContentRoot.Background = appThemeColorBrush;
+        }
+
+        private void PlayPauseButton_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            mediaPlayer.Play();
         }
     }
 }
