ded8b981abf3814157f77d9d2783911ddec86f03
David <zurek.dav@gmail.com>
2023-11-20 23:01:47 +0100

    Add UI for publishing a sound on PublishSoundPage
    - Add headers, descriptions, list of sounds to select from with filter text field, text field for name, text field for description and publish button
    - Add functionality for filter AutoSuggestBox

--- UniversalSoundBoard/Pages/PublishSoundPage.xaml.cs
+++ UniversalSoundBoard/Pages/PublishSoundPage.xaml.cs
@@ -1,4 +1,7 @@
-﻿using UniversalSoundboard.DataAccess;
+﻿using CommunityToolkit.WinUI.Collections;
+using System.Collections.ObjectModel;
+using UniversalSoundboard.DataAccess;
+using UniversalSoundboard.Models;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Media;
@@ -7,14 +10,28 @@ namespace UniversalSoundboard.Pages
 {
     public sealed partial class PublishSoundPage : Page
     {
+        private ObservableCollection<DialogSoundListItem> SoundItems;
+        private AdvancedCollectionView SoundsCollectionView;
+
         public PublishSoundPage()
         {
             InitializeComponent();
+
+            SoundItems = new ObservableCollection<DialogSoundListItem>();
+            SoundsCollectionView = new AdvancedCollectionView(SoundItems, true);
+
+            foreach (var sound in FileManager.itemViewHolder.AllSounds)
+                SoundItems.Add(new DialogSoundListItem(sound));
         }
 
         private void Page_Loaded(object sender, RoutedEventArgs e)
         {
             SetThemeColors();
+        }
+
+        private void FilterAutoSuggestBox_TextChanged(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs args)
+        {
+            SoundsCollectionView.Filter = item => ((DialogSoundListItem)item).Sound.Name.ToLower().Contains(sender.Text.ToLower());
         }
 
         private void SetThemeColors()
