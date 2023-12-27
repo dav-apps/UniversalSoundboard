76267e5c5140505df5fec772450f30d11d907c8c
David <zurek.dav@gmail.com>
2023-12-27 18:08:14 +0100

    Increase breakpoint for collapsed top buttons

--- UniversalSoundBoard/Common/Constants.cs
+++ UniversalSoundBoard/Common/Constants.cs
@@ -7,7 +7,7 @@ namespace UniversalSoundboard.Common
     {
         #region Design constants
         public const int mobileMaxWidth = 775;
-        public const int topButtonsCollapsedMaxWidth = 1600;
+        public const int topButtonsCollapsedMaxWidth = 1800;
         public const int sideBarCollapsedMaxWidth = 1100;
         public const int hideSearchBoxMaxWidth = 800;
         #endregion
