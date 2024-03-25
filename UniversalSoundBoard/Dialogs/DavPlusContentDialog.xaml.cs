74a3da5af9ff0c9fae1e97de235dfbec5569d41f
David <zurek.dav@gmail.com>
2024-03-26 00:10:29 +0100

    Implement basic custom ContentDialog for upgrading to UniversalSoundboard Plus

---
+++ UniversalSoundBoard/Dialogs/DavPlusContentDialog.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace UniversalSoundboard.Dialogs
+{
+    public sealed partial class DavPlusContentDialog : ContentDialog
+    {
+        public DavPlusContentDialog()
+        {
+            InitializeComponent();
+        }
+    }
+}
